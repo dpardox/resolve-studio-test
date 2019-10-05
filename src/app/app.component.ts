bf5154bd9e81fedf2aa93ac3233efe8a1e7b15ec
Steven Pardo <dspardo6@gmail.com>
2019-10-04 21:58:38 -0500

    Bulma.io framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'resolve-studio-test';
-}
+export class AppComponent { }
